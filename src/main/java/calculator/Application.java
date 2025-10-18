488315d2f1391d709927bb24b0826f0eb8e8eb68
patkrdj <taejun154@gmail.com>
2025-10-18 17:22:31 +0900

    feat: 문자열 추출 및 숫자 변환 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -25,5 +25,25 @@ public class Application {
             input = input.substring(index+2);
         }
 
+        // 문자열 추출 및 숫자 변환
+        for (String delimiter : delimiters) {
+            if (delimiter == null || delimiter.equals(delimiters[0])) continue;
+            input = input.replace(delimiter, delimiters[0]);
+        }
+
+        String[] tokens = input.split(delimiters[0]);
+        int[] values = new int[tokens.length];
+
+        for (int i = 0; i < tokens.length; i++) {
+            try{
+                values[i] = Integer.parseInt(tokens[i]);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("유효하지 않은 값이 입력되었습니다.");
+            }
+            if (values[i] <= 0) {
+                throw new IllegalArgumentException("양수가 아닌 값이 입력되었습니다.");
+            }
+        }
+
     }
 }
