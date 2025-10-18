1c58a1bf793d286927000126407dbe7eb3d1a029
patkrdj <taejun154@gmail.com>
2025-10-18 17:16:45 +0900

    feat: 구분자 인식 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -13,5 +13,17 @@ public class Application {
         }
         input = input.trim();
 
+        // 구분자 인식
+        String[] delimiters = {",", ":", null};
+        if (input.startsWith("//")) {
+            int index = input.indexOf("\\n");
+            if (index == -1) {
+                throw new IllegalArgumentException("커스텀 구분자를 확인할 수 없습니다.");
+            }
+            String customDelimiter = input.substring(2, index);
+            delimiters[2] = customDelimiter;
+            input = input.substring(index+2);
+        }
+
     }
 }
