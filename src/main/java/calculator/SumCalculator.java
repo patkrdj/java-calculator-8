ea65f95d1420b382f5443ec7ee4aca6fdfed2689
patkrdj <taejun154@gmail.com>
2025-10-20 20:04:30 +0900

    fix: sum 값을 연산할 때 overflow가 발생하면 Exception을 발생

--- src/main/java/calculator/SumCalculator.java
+++ src/main/java/calculator/SumCalculator.java
@@ -4,6 +4,9 @@ public class SumCalculator {
     public static int getSum(int[] values) {
         int sum = 0;
         for (int value : values) {
+            if (sum > Integer.MAX_VALUE - value) {
+                throw new IllegalArgumentException("Sum 값을 연산하는데 overflow가 발생하였습니다.");
+            }
             sum += value;
         }
         return sum;
